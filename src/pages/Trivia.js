e7e505f97a8e682b1ff68048ef9e5cfdaca4bab4
cjheyde <carlaheyde@gmail.com>
2022-05-25 16:45:01 -0300

    Grupo 19 - requisito 4 - parcial

--- src/pages/Trivia.js
+++ src/pages/Trivia.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Header from '../components/Header';
 
 class Trivia extends Component {
   render() {
     return (
-      <h1>
-        adad
-      </h1>
+      <header>
+        <Header />
+      </header>
     );
   }
 }
