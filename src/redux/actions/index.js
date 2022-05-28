19bc5b4c713a48a2cc6d98622e147177884ee136
cjheyde <carlaheyde@gmail.com>
2022-05-27 23:08:52 -0300

    criada action Set_time_answer e o timereducer

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,13 +1,14 @@
 export const SET_PLAYER_NAME = 'SET_PLAYER_NAME';
 export const SET_PLAYER_EMAIL = 'SET_PLAYER_EMAIL';
 export const TOKEN = 'TOKEN';
+export const SET_TIME_ANSWER = 'SET_TIME_ANSWER';
 
-export const savePlayerNameAction = (name) => ({
+export const savePlayerNameAct = (name) => ({
   type: SET_PLAYER_NAME,
   name,
 });
 
-export const savePlayerEmailAction = (gravatarEmail) => ({
+export const savePlayerEmailAct = (gravatarEmail) => ({
   type: SET_PLAYER_EMAIL,
   gravatarEmail,
 });
@@ -15,4 +16,9 @@ export const savePlayerEmailAction = (gravatarEmail) => ({
 export const saveTokenAction = (token) => ({
   type: TOKEN,
   token,
+});
+
+export const saveTimeAct = (secondsToStore) => ({
+  type: SET_TIME_ANSWER,
+  secondsToStore,
 });
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
 import player from './PlayerReducer';
 import token from './TokenReducer';
+import time from './TimeReducer';
 
-const rootReducer = combineReducers({ player, token });
+const rootReducer = combineReducers({ player, token, time });
 
 export default rootReducer;
