b6b7e8285509fea97c79d7e4a0b31b960e9792cf
devpbdias <devpbdias@gmail.com>
2022-05-30 18:50:42 -0300

    logo

--- src/components/LogoTrivia.js
+++ src/components/LogoTrivia.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import logo from '../images/trivia.png';
 
 export default class LogoTrivia extends Component {
   render() {
     return (
       <div>
         <img
-          src="./images/trivia.png"
+          src={ logo }
+          width="400px"
           alt="Logo do Jogo Trivia"
         />
       </div>
